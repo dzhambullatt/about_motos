52edb4b906e919a4fa35730c8b924cd5c590027f
djambulat <jambulatahmadov99@gmail.com>
2022-09-14 16:32:01 +0300

    updated models

--- moto/admin.py
+++ moto/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Moto)
+admin.site.register(Cats)
--- moto/models.py
+++ moto/models.py
@@ -4,6 +4,13 @@ from django.db import models
 class Cats(models.Model):
     name = models.CharField(max_length=150, verbose_name="Название")
 
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name ="Категория"
+        verbose_name_plural = "Категории"
+
+
 
 class Moto(models.Model):
     title = models.CharField(max_length=250, verbose_name="Заголовок")
@@ -14,4 +21,11 @@ class Moto(models.Model):
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
     cats = models.ForeignKey(Cats, on_delete=models.CASCADE)
 
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name ="Мотоцикл"
+        verbose_name_plural = "Мотоциклы"
 
