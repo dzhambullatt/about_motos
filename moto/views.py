12deb778613d9bd9d35509355c94c4a507569ce2
djambulat <jambulatahmadov99@gmail.com>
2022-09-14 12:45:30 +0300

    created new view

--- moto/views.py
+++ moto/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def news(request):
+    return HttpResponse('<p>hello world</p>')
+
+
+def about(request):
+    return HttpResponse('<h1>about page</h1>')
