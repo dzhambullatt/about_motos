52cf7ed1bfaebcb8c99f67f349b00cb2ad1cab7e
djambulat <jambulatahmadov99@gmail.com>
2022-09-15 12:47:08 +0300

    updated settings.py

--- moto/views.py
+++ moto/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Moto
+
 
 def news(request):
-    return render(request, 'moto/index.html')
+    mot = Moto.objects.order_by('-time_create')
+    context = {
+        'mot': mot,
+        'title': 'title'
+    }
+    return render(request, 'moto/index.html', context)
 
 
 def about(request):
