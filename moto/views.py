c14b33bc1fcda3389afdf53df67d1123620640cc
djambulat <jambulatahmadov99@gmail.com>
2022-09-16 17:45:08 +0300

    updated template

--- moto/views.py
+++ moto/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Moto
+from .models import *
 
 
 def news(request):
     mot = Moto.objects.order_by('-time_create')
+    cats = Cats.objects.all()
     context = {
         'mot': mot,
-        'title': 'title'
+        'title': 'Мотоциклы',
+        'cats': cats,
     }
     return render(request, 'moto/index.html', context)
 
