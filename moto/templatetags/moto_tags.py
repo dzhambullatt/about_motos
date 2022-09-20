8fe8566339c4133078334703211c8a04ed889698
djambulat <jambulatahmadov99@gmail.com>
2022-09-20 17:23:20 +0300

    created commentaries

---
+++ moto/templatetags/moto_tags.py
@@ -0,0 +1,10 @@
+# from django import template
+#
+# from moto.models import Cats
+#
+# register = template.Library()
+#
+#
+# @register.simple_tag()
+# def get_cats():
+#     return Cats.objects.all()
