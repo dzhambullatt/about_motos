99e081e7fbc84832a4de4ca9e9211af76ce5bad6
djambulat <jambulatahmadov99@gmail.com>
2022-09-21 17:20:35 +0300

    updated forms.py

--- moto/forms.py
+++ moto/forms.py
@@ -1,2 +1,9 @@
 from django import forms
 from .models import Cats
+
+
+class MotoForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    content = forms.CharField()
+    is_published = forms.BooleanField()
+    cats = forms.ModelChoiceField(queryset=Cats.objects.all())
