08afd875d37e7b6576695b3291fa58f0b57130c3
djambulat <jambulatahmadov99@gmail.com>
2022-09-21 19:36:59 +0300

    worked with template forms

--- moto/forms.py
+++ moto/forms.py
@@ -3,7 +3,8 @@ from .models import Cats
 
 
 class MotoForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    cats = forms.ModelChoiceField(queryset=Cats.objects.all())
+    title = forms.CharField(max_length=255, label='Название')
+    content = forms.CharField(label='Контент')
+    is_published = forms.BooleanField(label='Опубликовано', initial=True)
+    cats = forms.ModelChoiceField(empty_label='Выбрать Категорию', queryset=Cats.objects.all(), label='Категория')
+
